1b2461dd915fac713fbc20f3ad4791676ace08df
안알랴줌 <freesens@gmail.com>
2023-01-11 10:14:12 +0900

    ResultEnum 추가

--- src/main/java/com/tripmate/domain/common/dto/CodeDTO.java
+++ src/main/java/com/tripmate/domain/common/dto/CodeDTO.java
@@ -1,10 +1,13 @@
 package com.tripmate.domain.common.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+
+import javax.validation.constraints.NotBlank;
 
 @Builder
 @NoArgsConstructor
@@ -12,7 +15,11 @@ import lombok.NonNull;
 @Getter
 public class CodeDTO {
     @NonNull
+    @NotBlank(message = "공통코드를 입력하세요")
+    @Schema(description = "공통코드")
     private String commonCode;
     @NonNull
+    @NotBlank(message = "공통상세코드를 입력하세요")
+    @Schema(description = "공통상세코드")
     private String commonDetailCode;
 }
