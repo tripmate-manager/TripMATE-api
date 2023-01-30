6e7a564b520ddb635f7be50358c3f6ef230749c1
최지연 <cjyeon@pionnet.co.kr>
2023-01-30 13:33:40 +0900

    - 불필요 import 제거

--- src/main/java/com/tripmate/domain/members/dto/MemberMailDTO.java
+++ src/main/java/com/tripmate/domain/members/dto/MemberMailDTO.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
