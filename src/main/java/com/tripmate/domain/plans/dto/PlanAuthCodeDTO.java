d3b0e2359ffccd8d5f3cdb065efc54b253ea1705
최지연 <cjyeon@pionnet.co.kr>
2023-03-03 16:11:31 +0900

    - PlanAuthCodeDTO 수정

--- src/main/java/com/tripmate/domain/plans/dto/PlanAuthCodeDTO.java
+++ src/main/java/com/tripmate/domain/plans/dto/PlanAuthCodeDTO.java
@@ -1,6 +1,7 @@
 package com.tripmate.domain.plans.dto;
 
 
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,14 +16,17 @@ import javax.validation.constraints.NotBlank;
 @Getter
 @Schema(description = "플랜 인증코드 생성 Request DTO")
 public class PlanAuthCodeDTO {
+    @Hidden
     private int inviteCodeNo;
 
     @NotBlank(message = "플랜 번호를 입력해주세요.")
+    @Schema(description = "플랜 번호")
     private String planNo;
 
-    @NotBlank(message = "초대 타입 번호를 입력해주세요.")
+    @NotBlank(message = "초대 타입 코드를 입력해주세요.")
+    @Schema(description = "초대 타입 코드")
     private String inviteTypeCode;
 
-    @NotBlank(message = "초대 코드를 입력해주세요.")
+    @Hidden
     private String inviteCode;
 }
