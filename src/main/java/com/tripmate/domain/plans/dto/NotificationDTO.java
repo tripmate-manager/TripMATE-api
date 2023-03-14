978bbaa2bb6a2474ac3d7a38b5b73e61980feec2
최지연 <cjyeon@pionnet.co.kr>
2023-03-14 17:58:27 +0900

    - NotificationDTO > validation annotation 수정

--- src/main/java/com/tripmate/domain/plans/dto/NotificationDTO.java
+++ src/main/java/com/tripmate/domain/plans/dto/NotificationDTO.java
@@ -7,9 +7,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
+import java.util.List;
 
 @Builder
 @NoArgsConstructor
@@ -17,28 +18,25 @@ import javax.validation.constraints.Pattern;
 @Getter
 @Schema(description = "알림 DTO")
 public class NotificationDTO {
-    @Min(value = 1, message = "플랜 번호는 필수이며, 양수만 입력 가능합니다.")
+    @NotBlank(message = "플랜 번호를 입력해주세요.")
     @Schema(description = "플랜 번호")
-    private int planNo;
+    private String planNo;
 
-    @Schema(description = "데일리플랜 게시글 번호")
-    private int postNo;
+    @Schema(description = "데일리플랜 게시글 번호 (일정 알림인 경우 입력한 게시글에 대한 알람을 등록합니다.)")
+    private String postNo;
 
     @NotBlank(message = "알림타입코드를 입력해주세요.")
     @Pattern(regexp = "^[123]0$", message = "알림타입코드는 10, 20, 30만 입력 가능합니다.")
     @Schema(description = "알림타입코드(10: 여행일정알림, 20: 플랜리더변경알림, 30: 초대알림)")
     private String notificationTypeCode;
 
-    @Min(value = 1, message = "발신자 회원번호는 필수이며, 양수만 입력 가능합니다.")
+    @NotBlank(message = "발신자 회원번호를 입력해주세요.")
     @Schema(description = "발신자 회원번호")
-    private int senderNo;
+    private String senderNo;
 
-    @Min(value = 1, message = "수신자 회원번호는 필수이며, 양수만 입력 가능합니다.")
+    @NotEmpty(message = "수신자 회원번호를 입력해주세요.")
     @Schema(description = "수신자 회원번호")
-    private int receiverNo;
-
-    @Hidden
-    private String readDateTime;
+    private List<String> receiverNoList;
 
     @Hidden
     private String notificationDateTime;
