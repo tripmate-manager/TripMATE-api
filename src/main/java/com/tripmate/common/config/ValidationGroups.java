acdd4a85f7e5122dbc7bccffc9a5b2305c84f5fe
최지연 <cjyeon@pionnet.co.kr>
2023-04-20 16:31:06 +0900

    - ValidationGroups/ValidationSequence 추가

---
+++ src/main/java/com/tripmate/common/config/ValidationGroups.java
@@ -0,0 +1,7 @@
+package com.tripmate.common.config;
+
+public class ValidationGroups {
+    public interface NotBlankGroup {}
+    public interface NumberTypeGroup {}
+    public interface PatternCheckGroup {}
+}
