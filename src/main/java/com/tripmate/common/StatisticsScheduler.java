67f5624bfdd6b3537bd16db4e083ae19a684f723
최지연 <cjyeon@pionnet.co.kr>
2023-05-17 09:33:39 +0900

    - description 띄어쓰기 수정 - scheduler cron 시간 수정

--- src/main/java/com/tripmate/common/StatisticsScheduler.java
+++ src/main/java/com/tripmate/common/StatisticsScheduler.java
@@ -4,27 +4,29 @@ import com.tripmate.domain.common.ConstCode;
 import com.tripmate.domain.statistics.dao.StatisticsDAO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@EnableScheduling
 @RequiredArgsConstructor
 public class StatisticsScheduler {
     private final StatisticsDAO statisticsDAO;
 
-    @Scheduled(cron = "0 0 0 * * *")
+    @Scheduled(cron = "0 0 1 * * *")
     public void planLikeStatisticsScheduler() {
         statisticsDAO.insertPlanLikeStatistics(ConstCode.STATISTICS_TERM_CODE_1_MONTH);
         statisticsDAO.insertPlanLikeStatistics(ConstCode.STATISTICS_TERM_CODE_3_MONTH);
     }
 
-    @Scheduled(cron = "0 0 0 * * *")
+    @Scheduled(cron = "0 0 1 * * *")
     public void searchKeywordStatisticsScheduler() {
         statisticsDAO.insertSearchKeywordStatistics(ConstCode.STATISTICS_TERM_CODE_1_MONTH);
     }
 
-    @Scheduled(cron = "0 0 0 * * *")
+    @Scheduled(cron = "0 0 1 * * *")
     public void planAttributeStatisticsScheduler() {
         statisticsDAO.insertPlanAttributeStatistics(ConstCode.STATISTICS_TERM_CODE_1_MONTH);
     }
